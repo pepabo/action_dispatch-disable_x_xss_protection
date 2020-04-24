17ff4927e1f32bc8440ee8fd45d0d5dc9f854f02
Kohei Yamamoto <kymmt90@gmail.com>
2020-04-24 15:46:52 +0900

    Override the default value of the "X-XSS-Protection" header as "0"
    This gem overrides the default headers setting which Action Dispatch
    sets. The Configuration is executed during initializing Rails apps.

--- lib/action_dispatch/disable_x_xss_protection.rb
+++ lib/action_dispatch/disable_x_xss_protection.rb
@@ -1,7 +1,1 @@
 require "action_dispatch/disable_x_xss_protection/railtie"
-
-module ActionDispatch
-  module DisableXXssProtection
-    # Your code goes here...
-  end
-end
--- lib/action_dispatch/disable_x_xss_protection/railtie.rb
+++ lib/action_dispatch/disable_x_xss_protection/railtie.rb
@@ -1,6 +1,9 @@
 module ActionDispatch
   module DisableXXssProtection
     class Railtie < ::Rails::Railtie
+      initializer 'disable_x_xss_protection', after: 'action_dispatch.configure' do |app|
+        ActionDispatch::Response.default_headers = app.config.action_dispatch.default_headers.merge('X-XSS-Protection' => '0')
+      end
     end
   end
 end
